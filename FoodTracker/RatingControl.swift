688a5af5568fa2692554dfe004a2daaeef28d578
singleghost <570111267@qq.com>
2017-04-21 13:13:11 +0800

    Initial Commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 谢俊东 on 2017/4/21.
+//  Copyright © 2017年 zju. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
